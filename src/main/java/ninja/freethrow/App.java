c80a733f170c1fe4ddfe1ad33d3813f75156ed5c
Andy Aylward <aaylward@gmail.com>
2015-08-08 01:35:36 -0400

    add pom and App

---
+++ src/main/java/ninja/freethrow/App.java
@@ -0,0 +1,9 @@
+package ninja.freethrow;
+
+import com.google.common.base.Optional;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
