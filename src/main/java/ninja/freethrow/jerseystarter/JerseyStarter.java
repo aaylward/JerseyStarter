98cc647f705747ba39910b26df30f1ef4af7c8de
Andy Aylward <aaylward@gmail.com>
2015-08-11 10:03:27 -0400

    ok app root works now

---
+++ src/main/java/ninja/freethrow/jerseystarter/JerseyStarter.java
@@ -0,0 +1,55 @@
+package ninja.freethrow.jerseystarter;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+public class JerseyStarter {
+  private Server server;
+  private Configuration configuration;
+  private JerseyStarterResourceConfiguration jersey;
+
+  public static JerseyStarter newStarterApp(Configuration configuration) {
+    JerseyStarterResourceConfiguration jersey = new JerseyStarterResourceConfiguration()
+        .configure(configuration);
+    return new JerseyStarter(configuration, jersey);
+  }
+
+  public JerseyStarter(Configuration configuration, JerseyStarterResourceConfiguration jersey) {
+    this.configuration = configuration;
+    this.jersey = jersey;
+  }
+
+  public JerseyStarter buildServer() {
+    server = new Server(configuration.getPort());
+
+    ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    contextHandler.setContextPath(configuration.getAppRoot().orElse("/"));
+
+    server.setHandler(contextHandler);
+    contextHandler.addServlet(new ServletHolder(new ServletContainer(jersey)), "/*");
+    return this;
+  }
+
+  public void run() throws Exception {
+    run(true);
+  }
+
+  public void run(boolean join) throws Exception {
+    server.start();
+    if (join) {
+      server.join();
+    }
+  }
+
+  public void stop() throws Exception {
+    try {
+      server.stop();
+    } finally {
+      if (server != null) {
+        server.destroy();
+      }
+    }
+  }
+}
--- src/test/java/ninja/freethrow/jerseystarter/JerseyStarterTest.java
+++ src/test/java/ninja/freethrow/jerseystarter/JerseyStarterTest.java
@@ -6,31 +6,31 @@ import com.hubspot.horizon.HttpRequest.ContentType;
 import com.hubspot.horizon.HttpRequest.Method;
 import com.hubspot.horizon.HttpResponse;
 import com.hubspot.horizon.apache.ApacheHttpClient;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class JerseyStarterTest extends JerseyStarterApplication {
+public class JerseyStarterTest {
   private static final int TEST_PORT = 8090;
   private static final HttpClient HTTP = new ApacheHttpClient();
+  private static final String APP_ROOT = "/testing-is-fun";
 
   public JerseyStarterTest() throws Exception {
-    super();
     Configuration configuration = new Configuration(TEST_PORT,
         JerseyStarterTest.class.getPackage(),
-        Optional.of("test"),
+        Optional.of(APP_ROOT),
         0);
-    configure(configuration);
-    run(false);
+    JerseyStarter
+        .newStarterApp(configuration)
+        .buildServer()
+        .run(false);
   }
 
   @Test
-  @Ignore
   public void itCanServeRequests() {
-    String uri = String.format("http://localhost:%d/test/this-is-nice", TEST_PORT);
+    String uri = String.format("http://localhost:%d%s/this-is-nice", TEST_PORT, APP_ROOT);
     HttpRequest request = HttpRequest.newBuilder()
         .setUrl(uri)
         .setMethod(Method.GET)
