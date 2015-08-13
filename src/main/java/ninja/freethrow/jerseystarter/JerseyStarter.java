df65de4a0c089050c2c36f72ef8da98740247a7e
Andy Aylward <aaylward@gmail.com>
2015-08-13 12:35:34 -0400

    start this jersey2-guice business

--- src/main/java/ninja/freethrow/jerseystarter/JerseyStarter.java
+++ src/main/java/ninja/freethrow/jerseystarter/JerseyStarter.java
@@ -1,8 +1,13 @@
 package ninja.freethrow.jerseystarter;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+
 import ninja.freethrow.jerseystarter.config.StartupConfiguration;
 import ninja.freethrow.jerseystarter.providers.ObjectMapperProvider;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.jackson.JacksonFeature;
@@ -10,6 +15,8 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.inject.servlet.GuiceFilter;
 
 public class JerseyStarter implements Service {
   private static final Logger LOG = LoggerFactory.getLogger(JerseyStarter.class);
@@ -64,13 +71,21 @@ public class JerseyStarter implements Service {
   }
 
   private Server buildServer() {
+    ServletContainer servletContainer = new ServletContainer(jersey);
+
+    ServletHolder sh = new ServletHolder(servletContainer);
     Server server = new Server(port);
+    ServletContextHandler context = new ServletContextHandler(
+        ServletContextHandler.SESSIONS);
+    context.setContextPath(contextPath);
 
-    ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
-    contextHandler.setContextPath(contextPath);
+    FilterHolder filterHolder = new FilterHolder(GuiceFilter.class);
+    context.addFilter(filterHolder, DEFAULT_SERVLET_PATH_SPEC,
+        EnumSet.allOf(DispatcherType.class));
 
-    server.setHandler(contextHandler);
-    contextHandler.addServlet(new ServletHolder(new ServletContainer(jersey)), DEFAULT_SERVLET_PATH_SPEC);
+    context.addServlet(sh, DEFAULT_SERVLET_PATH_SPEC);
+
+    server.setHandler(context);
     return server;
   }
 
---
+++ src/main/java/ninja/freethrow/jerseystarter/config/BaseModule.java
@@ -0,0 +1,9 @@
+package ninja.freethrow.jerseystarter.config;
+
+import com.google.inject.AbstractModule;
+
+public class BaseModule extends AbstractModule {
+  @Override
+  public void configure() {
+  }
+}
